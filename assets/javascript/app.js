e59474d13c999a02845a464a3ee562c3a47bf0b6
jeffreyyourman <jeffreyyourman@gmail.com>
2016-07-03 14:13:47 -0400

    finished adding score and how the snake loses

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,6 +7,10 @@ var EMPTY = 0, SNAKE = 1, FRUIT =2;
 //directions
 var LEFT=0, UP=1, RIGHT=2, DOWN=3;
 
+//keyCodes
+
+var KEY_LEFT=37, KEY_UP=38, KEY_RIGHT=39, KEY_DOWN=40;
+
 var grid = {
     width: null,
     height: null,
@@ -73,22 +77,35 @@ function setFood() {
 
 //Game objects
 //ctx is context
-var canvas, ctx, keystate, frames;
+var canvas, ctx, keystate, frames, score;
 function main(){
+
+
   canvas = document.createElement('canvas');
   canvas.width = COLS*20;
   canvas.height = ROWS*20;
   ctx = canvas.getContext('2d');
   document.body.appendChild(canvas);
 
+  ctx.font = '12px Helvetica';
+
+
   frames = 0;
   keystate = {};
+  document.addEventListener('keydown', function(evt){
+    keystate[evt.keyCode] = true;
+  });
+
+  document.addEventListener('keyup', function(evt){
+    delete keystate[evt.keyCode];
+  });
 
   init();
   loop();
 }
 
 function init(){
+  score = 0;
   grid.init(EMPTY, COLS, ROWS);
 
   //sp is set stop position
@@ -108,6 +125,54 @@ function loop(){
 
 function update (){
   frames++;
+
+  if (keystate[KEY_LEFT] &&  snake.direction !== LEFT)
+    snake.direction = LEFT;
+  if (keystate[KEY_UP] &&  snake.direction !== UP)
+    snake.direction = UP;
+  if (keystate[KEY_RIGHT] &&  snake.direction !== RIGHT)
+    snake.direction = RIGHT;
+  if (keystate[KEY_DOWN] &&  snake.direction !== DOWN)
+    snake.direction = DOWN;
+  if (frames%5  === 0 ) {
+    var nx = snake.last.x;
+    var ny = snake.last.y;
+
+    switch (snake.direction) {
+      // what is nx and ny. i know its direction for x and y but what does n standfor?
+    case LEFT:
+      nx--;
+      break;
+    case UP:
+      ny--;
+      break;
+    case RIGHT:
+      nx++;
+      break;
+    case DOWN:
+      ny++;
+      break;
+
+    }
+    if (0 > nx || nx > grid.width -1 || 0 > ny || ny> grid.height - 1 || grid.get(nx,ny) === SNAKE) {
+      return init();
+    }
+
+    if (grid.get(nx,ny) === FRUIT) {
+      var tail = {x:nx, y:ny};
+      score++;
+      setFood();
+    } else {
+      var tail = snake.remove();
+      grid.set(EMPTY, tail.x, tail.y);
+      tail.x = nx;
+      tail.y = ny;
+    }
+
+    grid.set(SNAKE, tail.x, tail.y);
+
+    snake.insert(tail.x, tail.y);
+  }
 }
 
 function draw (){
@@ -132,6 +197,8 @@ function draw (){
       ctx.fillRect(x*tw, y*th, tw, th);
     }
   }
+  ctx.fillStyle = '#000';
+  ctx.fillText('SCORE: ' + score, 10, canvas.height-10);
 }
 
 main();
